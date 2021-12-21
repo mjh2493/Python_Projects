f49a47f5dfe965d3a44b63af5675a8cfe944c25e
mjh2493 <mjh2493@gmail.com>
2021-12-20 21:01:37 -0500

    inheritance_edit

--- inheritance.py
+++ inheritance.py
@@ -1,12 +1,15 @@
+#creates main class of student for a university
 class Student:
     name= 'Mallory'
     email= ''
     password= ''
 
+#creates a child class that has specific information for grad students
 class grad(Student):
     degree= 'Art History'
     undergrad_college= 'UGA'
 
+#creates a child class with specific information for undergrad students
 class undergrad(Student):
     degree= 'Education'
     high_school= 'Lambert'
