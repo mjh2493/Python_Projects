8129499201efa5cee3425853808890c6ade083c4
mjh2493 <mjh2493@gmail.com>
2021-12-20 21:00:15 -0500

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class Student:
+    name= 'Mallory'
+    email= ''
+    password= ''
+
+class grad(Student):
+    degree= 'Art History'
+    undergrad_college= 'UGA'
+
+class undergrad(Student):
+    degree= 'Education'
+    high_school= 'Lambert'
+    grad_date= 2016
